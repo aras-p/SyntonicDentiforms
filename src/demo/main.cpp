43bb60be0ba13817332036b970870424df0cf018
Aras Pranckevicius <aras@nesnausk.org>
2026-04-09 15:46:43 +0300

    Move externals impl to separate source file

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,21 +1,10 @@
 
-#if defined(__APPLE__)
-#define SOKOL_METAL
-#elif defined(_WIN32)
-#define SOKOL_D3D11
-#else
-#define SOKOL_GLCORE
-#endif
-
-#define SOKOL_IMPL
 #include "external/sokol_app.h"
 #include "external/sokol_gfx.h"
 #include "external/sokol_log.h"
 #include "external/sokol_glue.h"
 #include "external/sokol_time.h"
 #include "external/sokol_debugtext.h"
-#define SOKOL_GFX_UTILS_IMPL
-#include "external/sokol_gfx_utils.h"
 
 #include "external/stb_image.h"
 
